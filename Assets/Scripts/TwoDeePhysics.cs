26555822a2fa6e1590d4c713a35b1254a9505985
lordkuragari <lord.kuragari@gmail.com>
2015-01-13 17:58:30 -0800

    first push
    collision doesnt work at real time speeds, but works when steping
    through

---
+++ Assets/Scripts/TwoDeePhysics.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwoDeePhysics : MonoBehaviour {
+
+	public bool debug = false;
+
+	//basic properties, in units/second
+	float acceleration = 4f;
+	float maxSpeed = 150f;
+	float gravity = 6f;
+	float maxFall = 200f;
+	float jump = 200f;
+
+	//a layer make set in Start()
+	int layerMask;
+
+	Rect box;
+
+	Vector3 velocity;
+
+	bool grounded = false;
+	bool falling = false;
+
+
+	int horizontalRays = 6;
+	int verticalRays = 4;
+	float margin = .2f;
+
+
+	// Use this for initialization
+	void Start () {
+		layerMask = LayerMask.NameToLayer ("normalCollisions");
+		velocity = Vector3.zero;
+	}
+
+	void FixedUpdate()
+	{
+		box = new Rect 
+			(
+				collider.bounds.min.x,
+				collider.bounds.min.y,
+				collider.bounds.size.x,
+				collider.bounds.size.y
+			);
+
+
+		if (!grounded)
+						velocity = new Vector3 (velocity.x, Mathf.Max (velocity.y - gravity, -maxFall), velocity.z);
+		if (velocity.y < 0)
+						falling = true;
+		if (grounded || falling) 
+		{
+			Vector3 startPoint = new Vector3(box.xMin + margin,box.center.y, transform.position.z);
+			Vector3 endPoint = new Vector3(box.xMax - margin,box.center.y, transform.position.z);
+
+			RaycastHit hitInfo;
+			float distance = box.height/2 + (grounded? margin: Mathf.Abs((velocity.y) * Time.deltaTime));
+
+
+			bool connected = false;
+
+			for(int i = 0; i < verticalRays;++i)
+			{
+				float lerpAmount = (float) i/ (float) verticalRays -1;
+				Vector3 origin = Vector3.Lerp(startPoint,endPoint,lerpAmount);
+				Ray ray = new Ray(origin, Vector3.down);
+				Vector3 starter =startPoint;
+				starter.y -= velocity.y * Time.deltaTime;
+				Vector3 ender = starter;
+				ender.y -= distance;
+
+				connected = Physics.Raycast(ray, out hitInfo, distance, layerMask);
+				if(debug)
+				Debug.DrawLine(starter,ender,Color.green);
+
+				if(connected)
+				   {
+					grounded = true;
+					falling = false;
+					transform.Translate(Vector3.down * (hitInfo.distance - box.height/2));
+					velocity = new Vector3 (velocity.x,0,velocity.z);
+					break;
+				}
+			}
+
+				if(!connected)
+				{
+					grounded = false;
+				}
+
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate()
+	{
+		transform.Translate (velocity * Time.deltaTime);
+	}
+}
