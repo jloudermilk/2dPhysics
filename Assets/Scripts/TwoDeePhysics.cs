79886e49c2066bed4f2a8caf35328a8659f33157
Justin Loudermilk <lord.kuragari@gmail.com>
2015-01-20 07:58:47 -0800

    working ish

--- Assets/Scripts/TwoDeePhysics.cs
+++ Assets/Scripts/TwoDeePhysics.cs
@@ -46,7 +46,7 @@ public class TwoDeePhysics : MonoBehaviour {
 
 
 		if (!grounded)
-						velocity = new Vector3 (velocity.x, Mathf.Max ((velocity.y - (gravity)), -maxFall), velocity.z);
+						velocity.y = Mathf.Max ((velocity.y - (gravity)), -maxFall);
 		if (velocity.y < 0)
 						falling = true;
 		if (grounded || falling) 
@@ -55,7 +55,7 @@ public class TwoDeePhysics : MonoBehaviour {
 			Vector3 endPoint = new Vector3(box.xMax - margin,box.center.y, transform.position.z);
 
 			RaycastHit hitInfo;
-			float distance = box.height/2 + (grounded? margin: Mathf.Abs((velocity.y) * Time.deltaTime));
+			float distance = box.height/2 + (grounded? margin: Mathf.Abs((velocity.y) * (2*Time.deltaTime)));
 
 
 			bool connected = false;
@@ -68,7 +68,7 @@ public class TwoDeePhysics : MonoBehaviour {
 				ender.y += distance;
 
 				float lerpAmount = (float) i/ (float) (verticalRays -1);
-				Vector3 origin = Vector3.Lerp(starter,endPoint,lerpAmount);
+				Vector3 origin = Vector3.Lerp(startPoint,endPoint,lerpAmount);
 				Ray ray = new Ray(origin, Vector3.down);
 			
 
@@ -91,6 +91,94 @@ public class TwoDeePhysics : MonoBehaviour {
 					grounded = false;
 				}
 
+		}
+		float horizontalAxis = Input.GetAxisRaw("Horizontal");
+
+		float newVelocityX = velocity.x;
+		if(horizontalAxis != 0)
+		{
+			newVelocityX += acceleration * horizontalAxis;
+			newVelocityX = Mathf.Clamp(newVelocityX,-maxSpeed,maxSpeed);
+		}
+		else if(velocity.x != 0)
+		{
+			int modifiter = velocity.x > 0? -1: 1;
+			newVelocityX += acceleration * modifiter;
+		}
+
+		velocity.x = newVelocityX;
+		if(velocity.x !=0)
+		{
+			Vector3 startPoint = new Vector3(box.center.x,box.yMin + margin, transform.position.z);
+			Vector3 endPoint = new Vector3(box.center.x,box.yMax - margin, transform.position.z);
+
+			RaycastHit hitInfo;
+
+			float sideRayLength = (box.width/2) + Mathf.Abs(newVelocityX * Time.deltaTime);
+			Vector3 direction =  newVelocityX > 0 ? Vector3.right: Vector3.left;
+
+			bool connected = false;
+
+			for(int i = 0; i < horizontalRays; ++i)
+			{
+				float lerpAmout = ((float) i / (float)(horizontalRays - 1));
+				Vector3 origin = Vector3.Lerp(startPoint,endPoint,lerpAmout);
+				Ray ray = new Ray(origin,direction);
+
+				connected = Physics.Raycast(ray,out hitInfo,sideRayLength);
+				if(debug)
+					Debug.DrawRay(origin,direction,Color.green);
+				if(connected)
+				{
+					transform.Translate(direction *(hitInfo.distance - box.width/2));
+					velocity.z =0;
+					break;
+				}
+			}
+		}
+		float verticalAxis = Input.GetAxisRaw("Vertical");
+		
+		float newVelocityZ = velocity.z;
+		if(verticalAxis != 0)
+		{
+			newVelocityZ += acceleration * verticalAxis;
+			newVelocityZ = Mathf.Clamp(newVelocityZ,-maxSpeed,maxSpeed);
+		}
+		else if(velocity.z != 0)
+		{
+			int modifiter = velocity.z > 0? -1: 1;
+			newVelocityZ += acceleration * modifiter;
+		}
+		
+		velocity.z = newVelocityZ;
+		if(velocity.x !=0)
+		{
+			Vector3 startPoint = new Vector3(box.center.x,box.yMin + margin, transform.position.z);
+			Vector3 endPoint = new Vector3(box.center.x,box.yMax - margin, transform.position.z);
+			
+			RaycastHit hitInfo;
+			
+			float sideRayLength = (box.width/2) + Mathf.Abs(newVelocityZ * Time.deltaTime);
+			Vector3 direction =  newVelocityZ > 0 ? Vector3.forward: Vector3.back;
+			
+			bool connected = false;
+			
+			for(int i = 0; i < horizontalRays; ++i)
+			{
+				float lerpAmout = ((float) i / (float)(horizontalRays - 1));
+				Vector3 origin = Vector3.Lerp(startPoint,endPoint,lerpAmout);
+				Ray ray = new Ray(origin,direction);
+				
+				connected = Physics.Raycast(ray,out hitInfo,sideRayLength);
+				if(debug)
+					Debug.DrawRay(origin,direction,Color.green);
+				if(connected)
+				{
+					transform.Translate(direction *(hitInfo.distance - box.width/2));
+					velocity.z =0;
+					break;
+				}
+			}
 		}
 	}
 
